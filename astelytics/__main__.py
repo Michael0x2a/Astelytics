85a389a8e09970eeecc389cf96cf3f8fea94180e
Michael Lee <michael.lee.0x2a@gmail.com>
2014-01-25 11:25:52 -0800

    Initial commit

---
+++ astelytics/__main__.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import collections
+import functools
+import copy
+
+import flask
+from flask.ext.classy import FlaskView, route
+from flask_sockets import Sockets
+
+
+app = flask.Flask(__name__, static_folder='static')
+sockets = Sockets(app)
+
+db = {}
+
+
+
+def support_jsonp(json):
+    callback = flask.request.args.get('callback', False)
+    if callback:
+        content = str(callback) + '(' + json + ')'
+        return flask.current_app.response_class(content, mimetype='application/json')
+    else:
+        return json
+    
+class SurveyView(FlaskView):
+    def __init__(self):
+        super(FlaskView, self).__init__()
+        self.question_id_counter = 0
+        
+    def index(self):
+        output = ['<!DOCTYPE html><html><head><title>Active surveys</title></head>']
+        output.append('<body><h1>Active Surveys</h1><ul>')
+        for survey_id in db:
+            output.append('<li><a href="/survey/{0}">{0}</a></lu>'.format(survey_id))
+        output.append('</ul></body></html>')
+        return '\n'.join(output)
+        
+    def get(self, survey_id):
+        survey_id = int(survey_id)
+        if survey_id not in db:
+            return 'Error -- Not found.'
+        else:
+            output = output = ['<!DOCTYPE html><html><head>']
+            output.append('<title>Survey {0}</title></head>'.format(survey_id))
+            output.append('<body><h1>Survey {0}</h1><ul>'.format(survey_id))
+            for question, answer in db[survey_id].items():
+                output.append('<li>{0}: {1}</li>'.format(question, answer))
+            output.append('</ul></body></html>')
+            return '\n'.join(output)
+            
+    def analyze(self, survey_id):
+        survey_id = int(survey_id)
+        if survey_id not in db:
+            return {}
+        else:
+            output = copy.deepcopy(db[survey_id])
+            for question, response in db[survey_id].items():
+                output[question]['data'] = collections.Counter(response['data'])
+            json = flask.json.dumps(output)
+            return support_jsonp(json)
+            
+    def visualize(self, survey_id):
+        survey_id = int(survey_id)
+        return flask.render_template('report.html', responses=db[survey_id])
+            
+    def post(self, survey_id):
+        survey_id = int(survey_id)
+        if survey_id not in db:
+            db[survey_id] = {}
+        attempts = ['bar', 'pie']
+        for value, response in flask.request.form.items():
+            if value not in db[survey_id]:
+                if len(attempts) == 0:
+                    attempt = 'bar'
+                else:
+                    attempt = attempts.pop()
+                db[survey_id][value] = {
+                    'display': {
+                        'default': attempt,
+                        'height': 300,
+                        'width': 400
+                    },
+                    'data': [],
+                    'unique-id': "question-{0}".format(self.question_id_counter),
+                }
+                self.question_id_counter += 1
+                
+            db[survey_id][value]['data'].append(response)
+        return 'Done, {0}'.format(survey_id)
+    
+if __name__ == '__main__':
+    SurveyView.register(app)
+    app.run(debug=True)
---
+++ astelytics/static/visualize.js
@@ -0,0 +1,100 @@
+function makeBarChart() {
+    'use strict';
+    
+    var chart = nv.models.discreteBarChart()
+        .x(function (d) { return d.label; })
+        .y(function (d) { return d.value; })
+        .staggerLabels(false)
+        .tooltips(true)
+        .showValues(true);
+        
+    nv.utils.windowResize(chart.update);
+    nv.addGraph(chart);
+    
+    return chart;
+}
+
+function makePieChart() {
+    'use strict';
+    
+    var chart = nv.models.pieChart()
+        .x(function (d) { return d.label; })
+        .y(function (d) { return d.value; })
+        .showLabels(true);
+        
+    nv.utils.windowResize(chart.update);
+    nv.addGraph(chart);
+    
+    return chart;
+}
+
+function updateChart(target, chart, data) {
+    'use strict';
+    
+    d3.select(target)
+        .datum(data)
+        .transition().duration(500)
+        .call(chart);
+        
+    return chart;
+}
+
+function formatToBarChartData(question, data) {
+    'use strict';
+    
+    return [{
+        key: question,
+        values: formatToPieChartData(question, data)
+    }];
+}
+
+function formatToPieChartData(question, data) {
+    'use strict';
+    var arr = [];
+    var i;
+    for (i in data) {
+        arr.push({ "label": i, "value": data[i] });
+    }
+    
+    return arr;
+}
+
+function setChartUpdate() {
+    'use strict';
+    
+    var mapping = {
+        'bar': [makeBarChart, formatToBarChartData],
+        'pie': [makePieChart, formatToPieChartData]
+    };
+    
+    
+    var charts = {};
+    
+    $('.response').each(function (index, elem) {
+        var chartType = $(elem).attr('class').split(' ')[1];
+        var chart = mapping[chartType][0]();
+        
+        charts[$(elem).attr('id')] = chart;
+    });
+    
+    setInterval(function() {
+        $.ajax({
+            url: 'http://localhost:5000/survey/analyze/1', 
+            dataType: "json",
+            success: function(json) {
+                var question;
+                for (question in json) {
+                    var response = json[question];
+                    var formatter = mapping[response['display']['default']][1];
+                    var formatted = formatter(question, response['data']);
+                    
+                    var unique_id = response['unique-id'];
+                    updateChart('#' + unique_id + ' svg', charts[unique_id], formatted);
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                alert(textStatus + " | " + errorThrown);
+            }
+        });
+    }, 500);
+}
