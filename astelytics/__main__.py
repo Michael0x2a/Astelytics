e21ed51ba409f1d2c767f3bd08fc6b2f3a528651
Michael Lee <michael.lee.0x2a@gmail.com>
2014-01-25 14:47:22 -0800

    Added word cloud

--- astelytics/__main__.py
+++ astelytics/__main__.py
@@ -4,6 +4,7 @@
 import collections
 import functools
 import copy
+import re
 
 import flask
 from flask.ext.classy import FlaskView, route
@@ -11,6 +12,7 @@ from flask_sockets import Sockets
 
 
 app = flask.Flask(__name__, static_folder='static')
+app.secret_key = ',*\xee\xd6tJ1Ja\xc8D\x9d!-\xa2k\xb6K\x9e\xb8\xff\xd7z\xc3'
 sockets = Sockets(app)
 
 # This is our "fake" database.
@@ -26,11 +28,21 @@ def support_jsonp(json):
         return flask.current_app.response_class(content, mimetype='application/json')
     else:
         return json
+        
+def rank_words(words, n):
+    boring = {'the', 'of', 'a', 'an', 'or', 'some', 'will', 'and', 'for', 
+        'should', 'would', 'did', 'does', 'do', 'but', 'yet', 'nor', 'it', 'was', ''}
+    clean = re.sub('[.,-;:]', ' ', words.lower())
+    clean = re.sub('[^a-z0-9 ]', '', clean)
+    clean = [a for a in clean.split(' ') if a not in boring] 
+    # I can't convert 'clean' to a set, since then it would squash duplicate 
+    # occurances of a word.
+    return collections.Counter(clean).most_common(n)
     
 class SurveyView(FlaskView):
     def __init__(self):
         super(FlaskView, self).__init__()
-        self.question_id_counter = 0
+        self.question_id_counter = 0   # An impromptu hash
         
     def index(self):
         output = ['<!DOCTYPE html><html><head><title>Active surveys</title></head>']
@@ -41,17 +53,7 @@ class SurveyView(FlaskView):
         return '\n'.join(output)
         
     def get(self, survey_id):
-        survey_id = int(survey_id)
-        if survey_id not in db:
-            return 'Error -- Not found.'
-        else:
-            output = output = ['<!DOCTYPE html><html><head>']
-            output.append('<title>Survey {0}</title></head>'.format(survey_id))
-            output.append('<body><h1>Survey {0}</h1><ul>'.format(survey_id))
-            for question, answer in db[survey_id].items():
-                output.append('<li>{0}: {1}</li>'.format(question, answer))
-            output.append('</ul></body></html>')
-            return '\n'.join(output)
+        return flask.render_template('submit.html')
             
     @route('<survey_id>/analytics')
     def analytics(self, survey_id):
@@ -61,39 +63,53 @@ class SurveyView(FlaskView):
         else:
             output = copy.deepcopy(db[survey_id])
             for question, response in db[survey_id].items():
-                output[question]['data'] = collections.Counter(response['data'])
+                if output[question]['data-type'] in ('bar', 'pie'):
+                    output[question]['data'] = collections.Counter(response['data'])
+                elif output[question]['data-type'] == 'text':
+                    output[question]['data'] = rank_words(' '.join(response['data']), 30)
             json = flask.json.dumps(output)
             return support_jsonp(json)
             
     @route('<survey_id>/report')
     def report(self, survey_id):
         survey_id = int(survey_id)
-        return flask.render_template('report.html', responses=db[survey_id])
+        responses = db[survey_id] if survey_id in db else {}
+        return flask.render_template('report.html', responses=responses)
             
     def post(self, survey_id):
         survey_id = int(survey_id)
         if survey_id not in db:
             db[survey_id] = {}
-        attempts = ['bar', 'pie']
+        attempts = ['text', 'bar', 'pie']
         for value, response in flask.request.form.items():
             if value not in db[survey_id]:
-                if len(attempts) == 0:
-                    attempt = 'bar'
-                else:
-                    attempt = attempts.pop()
-                db[survey_id][value] = {
-                    'display': {
-                        'default': attempt,
-                        'height': 300,
-                        'width': 400
-                    },
-                    'data': [],
-                    'unique-id': "question-{0}".format(self.question_id_counter),
-                }
-                self.question_id_counter += 1
+                self._create_new_question(survey_id, value, response, attempts);
                 
             db[survey_id][value]['data'].append(response)
-        return 'Done, {0}'.format(survey_id)
+            
+        flask.flash('Submitted')
+        return flask.redirect('survey')
+        
+    def _create_new_question(self, survey_id, value, response, attempts):
+        if len(attempts) == 0:
+            attempt = 'bar'
+        else:
+            attempt = attempts.pop()
+            
+        db[survey_id][value] = {
+            'display': {
+                'default': attempt,
+                'height': 300,
+                'width': 400
+            },
+            'data': [],
+            'data-type': self._determine_data_type(attempt),
+            'unique-id': "question-{0}".format(self.question_id_counter),
+        }
+        self.question_id_counter += 1
+        
+    def _determine_data_type(self, attempt):
+        return attempt
     
 if __name__ == '__main__':
     SurveyView.register(app)
--- astelytics/static/visualize.js
+++ astelytics/static/visualize.js
@@ -14,6 +14,15 @@ function makeBarChart() {
     return chart;
 }
 
+function formatToBarChartData(question, data) {
+    'use strict';
+    
+    return [{
+        key: question,
+        values: formatToPieChartData(question, data)
+    }];
+}
+
 function makePieChart() {
     'use strict';
     
@@ -28,26 +37,6 @@ function makePieChart() {
     return chart;
 }
 
-function updateChart(target, chart, data) {
-    'use strict';
-    
-    d3.select(target)
-        .datum(data)
-        .transition().duration(500)
-        .call(chart);
-        
-    return chart;
-}
-
-function formatToBarChartData(question, data) {
-    'use strict';
-    
-    return [{
-        key: question,
-        values: formatToPieChartData(question, data)
-    }];
-}
-
 function formatToPieChartData(question, data) {
     'use strict';
     var arr = [];
@@ -59,12 +48,89 @@ function formatToPieChartData(question, data) {
     return arr;
 }
 
+function makeWordCloud() {
+    'use strict';
+    var chart = d3.layout.cloud()
+        //.words(data)
+        .padding(5)
+        .rotate(function() { return ~~(Math.random() * 2) * 0; })
+        .font("Impact")
+        .fontSize(function(d) { return d.size; });
+        
+    chart.kind = 'word cloud';
+    return chart;
+}
+
+function updateWordCloud(target, chart, data) {
+    'use strict';
+    
+    d3.select(target).text('');
+
+    var fill = d3.scale.category20();
+    function draw(words) {
+        d3.select(target)
+            .attr("width", 400)
+            .attr("height", 300)
+          .append("g")
+            .attr("transform", "translate(150,150)")
+          .selectAll("text")
+            .data(words)
+          .enter().append("text")
+            .style("font-size", function(d) { return d.size + "px"; })
+            .style("font-family", "Impact")
+            .style("fill", function(d, i) { return fill(i); })
+            .attr("text-anchor", "middle")
+            .attr("transform", function(d) {
+              return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            })
+            .text(function(d) { return d.text; });
+    }
+            
+    chart = chart
+        .words(data)
+        .on("end", draw)
+        .start();    
+        
+    return chart
+}
+
+function formatToWordCloudData(target, data) {
+    return data.map(function (t) { 
+        return {
+            "text": t[0],
+            "size": 10 + t[1] * 10
+        };
+    });
+}
+
+function updateChart(target, chart, data) {
+    'use strict';
+    
+    if (chart.kind === 'word cloud') {
+        var temp = data.map(function (x) { return x[0] + x[1]}).join();;
+        if (chart.previous !== temp) {
+            updateWordCloud(target, chart, data);
+            
+            chart.previous = temp;
+        }
+        return chart;
+    }
+    
+    d3.select(target)
+        .datum(data)
+        .transition().duration(500)
+        .call(chart);
+        
+    return chart;
+}
+
 function setChartUpdate() {
     'use strict';
     
     var mapping = {
         'bar': [makeBarChart, formatToBarChartData],
-        'pie': [makePieChart, formatToPieChartData]
+        'pie': [makePieChart, formatToPieChartData],
+        'text': [makeWordCloud, formatToWordCloudData]
     };
     
     
@@ -93,7 +159,7 @@ function setChartUpdate() {
                 }
             },
             error: function(jqXHR, textStatus, errorThrown) {
-                alert(textStatus + " | " + errorThrown);
+                //alert(textStatus + " | " + errorThrown);
             }
         });
     }, 500);
